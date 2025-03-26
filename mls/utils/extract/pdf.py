a6fb7920d2d37285c967bd231fd518a36bcfe659
Oliver Zander <zander@cosmocode.de>
2025-03-26 13:29:18 +0100

    updated pdfminer

--- mls/utils/extract/pdf.py
+++ mls/utils/extract/pdf.py
@@ -1,24 +1,3 @@
-from pdfminer.pdfpage import PDFPage
-from pdfminer.pdftypes import dict_value, resolve1
-from pdfminer import high_level
+from pdfminer.high_level import extract_text
 
-
-class FixedPDFPage(PDFPage):
-    """
-    See https://github.com/pdfminer/pdfminer.six/pull/1027
-    """
-
-    def __init__(self, doc, page, attrs, label):
-        attrs = dict_value(attrs)
-
-        if media_box := attrs.get('MediaBox'):
-            attrs['MediaBox'] = resolve1(media_box)
-        else:
-            attrs['MediaBox'] = 0, 0, 612, 792  # US Letter
-
-        PDFPage.__init__(self, doc, page, attrs, label)
-
-
-high_level.PDFPage = FixedPDFPage
-
-extract = high_level.extract_text
+extract = extract_text
